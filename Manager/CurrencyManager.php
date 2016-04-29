d2e3f00fa8c8e8ad1c87d3d4ecf27924809d059d
Adam Piotrowski <adam@wellcommerce.org>
2016-04-29 13:53:16 +0200

    Changed extension configurations

--- Manager/CurrencyManager.php
+++ Manager/CurrencyManager.php
@@ -10,15 +10,15 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\Bundle\CurrencyBundle\Manager\Front;
+namespace WellCommerce\Bundle\CurrencyBundle\Manager;
 
-use WellCommerce\Bundle\CoreBundle\Manager\Front\AbstractFrontManager;
+use WellCommerce\Bundle\CoreBundle\Manager\AbstractManager;
 
 /**
  * Class CurrencyManager
  *
  * @author  Adam Piotrowski <adam@wellcommerce.org>
  */
-class CurrencyManager extends AbstractFrontManager
+class CurrencyManager extends AbstractManager
 {
 }
